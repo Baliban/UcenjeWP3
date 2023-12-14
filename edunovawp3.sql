bb430c1f8597d82bfcd8001eb047163378920874
Bartol <bartol567@gmail.com>
2023-12-14 20:03:14 +0100

    inicijalna verzija i veza izmedju tablica

---
+++ edunovawp3.sql
@@ -0,0 +1,31 @@
+
+
+
+-- ovo je komentar
+--create database edunovawp3;
+
+--drop database edunovawp3;
+--use master
+--use edunovawp3;		
+--create table smjerovi(
+--sifra int,
+--naziv varchar(50),
+--trajanje int,
+--cijena decimal(18,2),
+--vaucer bit
+--);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
