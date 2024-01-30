9171385b812f3471e67632ed9491bea1933214df
Baliban Mario <mariobalibanos@gmail.com>
2024-01-30 16:49:26 +0100

    Update raspored.sql

--- SQL/raspored.sql
+++ SQL/raspored.sql
@@ -1,3 +1,5 @@
+use master
+go
 drop database if exists Raspored;
 go
 create database Raspored;
@@ -25,21 +27,85 @@ CREATE TABLE odjel(
 
 alter table djelatnici add foreign key (odjel) references odjel(ID);
 alter table slike add foreign key (djelatnik) references djelatnici(ID);
-alter table odjel add foreign key (djelatnici) references djelatnici(ID);
-
-insert into djelatnici (ime,prezime,odjel) values
-('','',1),
-('','',1),
-('','',1),
-('','',2),
-('','',2);
 
 insert into odjel (vrsta) values
-('mesnica'),
-('delikatesa'),
-('prijem'),
-('sala');
+('Mesnica'),
+('Delikatesa'),
+('Voditelj'),
+('Sala');
 
-insert into slike (djelatnik,redni_br,putanja) values
-('',1,'')
+
+insert into djelatnici (ime,prezime,odjel) values
+('Mario','Baliban',1),
+('Durdica','Novakovic',1),
+('Danijela','Brezovac',2),
+('Jelena','Predic',2),
+('Snjezana','Kanski',2),
+('Manuela','Marusic',3),
+('Marijana','Iles',3),
+('Valentina','Ivkovic',3),
+('Snjezana','Polanec',4),
+('Martina','Gomercic',4),
+('Zeljka','Blazevic',4),
+('Romana','Pavlovic',4),
+('Zeljka','Milakovic',4);
+
+
+
+--insert into slike (djelatnik,redni_br,putanja) values
+--('',1,'')
+
+
+
+CREATE TABLE Raspored (
+		[Datum] DATE ,
+		[Dan] char(10) ,
+		Djelatnik int ,
+		Odjel int )
+
+alter table Raspored add foreign key (Djelatnik) references djelatnici(ID);
+alter table Raspored add foreign key (Odjel) references odjel(ID);
+
+		SET DATEFIRST 1
+
+DECLARE @StartDate AS DATETIME
+DECLARE @EndDate AS DATETIME
+SET @StartDate = CURRENT_TIMESTAMP
+SET @EndDate =  DATEADD(d, 6, @StartDate)
+WHILE @StartDate <= @EndDate
+
+BEGIN
+
+INSERT INTO Raspored
+
+([Dan],[Datum])
+
+SELECT
+
+CASE DATEPART(DW, @StartDate)
+
+WHEN 1 THEN 'Monday'
+WHEN 2 THEN 'Tuesday'
+WHEN 3 THEN 'Wednesday'
+WHEN 4 THEN 'Thursday'
+WHEN 5 THEN 'Friday'
+WHEN 6 THEN 'Saturday'
+WHEN 7 THEN 'Sunday'
+
+END AS DayOfWeek
+
+,@StartDate AS ActualDate
+
+SET @StartDate = DATEADD(DD, 1, @StartDate)
+
+END
+
+GO
+
+--use master
+--drop database Raspored
+--drop TABLE Raspored;
+
+--select * from Raspored;
+
 
